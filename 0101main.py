84fa4f03862efcc46084737b276e886b13a32d42
joaquinballesteros <jballesteros@uma.es>
2023-02-02 13:02:31 +0100

    File CSV name changed after generate

--- 0101main.py
+++ 0101main.py
@@ -45,10 +45,10 @@ def callBatch(fm,n_cores,n_task,min_id,max_id):
 #SBATCH --array=0-'''+str(n_task)+'''%32
 
 # the program to execute with its parameters:
+module load python/3.9.13
 source $HOME/fm_solver/envpypy/bin/activate
 time python $HOME/fm_solver/01main.py $HOME/fm_models/simples/'''+fm+".uvl "+str(n_cores) + " " + str(n_task) + " ${SLURM_ARRAY_TASK_ID} " + str(min_id) + " " + str(max_id) + '''
 deactivate"'''
-    print(a)
     text_file = open("script_" + fm + "_" + str(n_cores) + "_" + str(n_task) + "_" + str(min_id) + "_" + str(max_id)+".sh", "w")
  
 #write string to file
@@ -72,5 +72,9 @@ if __name__ == '__main__':
         spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
         for row in spamreader:
             callBatch(args.feature_model,args.n_cores,args.n_task,row[0],row[1])
+        csvfile.close()
+    os.rename(args.division_file,args.division_file+"_generated")
+
+    
 
 
