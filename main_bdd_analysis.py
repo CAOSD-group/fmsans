7fabfbead61d1f490fb6e0935f378dc52d5e2265
José Miguel Horcas <horcas@uma.es>
2023-02-10 22:19:51 +0100

    BDD analysis updated

--- main_bdd_analysis.py
+++ main_bdd_analysis.py
@@ -1,19 +1,19 @@
 import os
 import argparse
 
-from flamapy.metamodels.fm_metamodel.transformations import UVLReader, UVLWriter
+from flamapy.metamodels.fm_metamodel.transformations import UVLReader
 
 from flamapy.metamodels.bdd_metamodel.transformations import FmToBDD
 from flamapy.metamodels.bdd_metamodel.operations import (
-    BDDProductsNumber, 
-    BDDProductDistribution,
-    BDDFeatureInclusionProbability
+    BDDProductsNumber,
+    BDDCoreFeatures,
+    BDDDeadFeatures
 )
 
-from fm_solver.models.feature_model import FM
-from fm_solver.utils import utils, timer, memory_profiler
+from fm_solver.utils import timer, memory_profiler, sizer
 
-CODES = ['Reading', 'Transformation', 'Configs_op']
+
+CODES = ['Reading', 'Transformation', 'ConfigNumber_op', 'CoreFeatures_op', 'DeadFeatures_op']
 
 
 def main(fm_filepath: str) -> None:
@@ -23,46 +23,40 @@ def main(fm_filepath: str) -> None:
     # Load the feature model
     with memory_profiler.MemoryProfiler(name=CODES[0], logger=None), timer.Timer(name=CODES[0], logger=None):
         feature_model = UVLReader(fm_filepath).transform()
-    fm = FM.from_feature_model(feature_model)
+
+    fm_memory = sizer.getsizeof(feature_model, logger=None)
 
     # Create the BDD from the FM
     with memory_profiler.MemoryProfiler(name=CODES[1], logger=None), timer.Timer(name=CODES[1], logger=None):
-        bdd_model = FmToBDD(fm).transform()
+        bdd_model = FmToBDD(feature_model).transform()
+
+    bddmodel_memory = sizer.getsizeof(bdd_model, logger=None)
 
     # Products numbers
     with memory_profiler.MemoryProfiler(name=CODES[2], logger=None), timer.Timer(name=CODES[2], logger=None):
         n_configs = BDDProductsNumber().execute(bdd_model).get_result()
-    print(f'#Configs: {n_configs} ({utils.int_to_scientific_notation(n_configs)})')
+    print(f'#Configs: {n_configs}')
+
+    # Core features
+    with memory_profiler.MemoryProfiler(name=CODES[3], logger=None), timer.Timer(name=CODES[3], logger=None):
+        core_features = BDDCoreFeatures().execute(bdd_model).get_result()
+    print(f'Core features: {len(core_features)} {core_features}')
+
+    # Dead features
+    with memory_profiler.MemoryProfiler(name=CODES[4], logger=None), timer.Timer(name=CODES[4], logger=None):
+        dead_features = BDDDeadFeatures().execute(bdd_model).get_result()
+    print(f'Dead features: {len(dead_features)} {dead_features}')
 
     # Print outputs
-    header = f"#Configs,{','.join(c + '(s)' for c in CODES)},{','.join(c + '(B)' for c in CODES)}"
-    values = f'{n_configs},'
+    header = f"Features,Constraints,FM_size(B),BDD_size(B),{','.join(c + '(s)' for c in CODES)},{','.join(c + '(B)' for c in CODES)},#Configs,#Cores,Cores,#Deads,Deads"
+    values = f'{len(feature_model.get_features())},{len(feature_model.get_constraints())},{fm_memory},{bddmodel_memory},'
     values += ','.join([str(timer.Timer.timers[c]) for c in CODES])
     values += ',' + ','.join([str(memory_profiler.MemoryProfiler.memory_profilers[c]) for c in CODES])
+    values += f',{n_configs}'
+    values += f',{len(core_features)},"{core_features}"'
+    values += f',{len(dead_features)},"{dead_features}"'
     print(header)
     print(values)
-
-    # prod_dist = BDDProductDistribution().execute(bdd_model).get_result()
-    # print(f'#Configs (prod_dist): {sum(prod_dist)}')
-
-    # feat_prob = BDDFeatureInclusionProbability().execute(bdd_model).get_result()
-    # for f, p in feat_prob.items():
-    #     if p == 0:
-    #         print(f)
-    #         feature = fm.get_feature_by_name(f)
-    #         if feature is not None:
-    #             fm = fm_utils.deletion_feature(fm, f)
-
-    # constraints = [ctc for ctc in fm.get_constraints()]
-    # for ctc in fm.get_constraints():
-    #     features = ctc.get_features()
-    #     if any(fm.get_feature_by_name(f) is None for f in features):
-    #         constraints.remove(ctc)
-    
-    # fm.ctcs = constraints
-
-    # output_fm_filepath = f'{fm_name}_clean.uvl'
-    # UVLWriter(path=output_fm_filepath, source_model=fm).transform()
 
 
 if __name__ == '__main__':
