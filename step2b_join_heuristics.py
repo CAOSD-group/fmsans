01ed567af0c2709b61971120fea6ed1d27e61bcb
José Miguel Horcas <horcas@uma.es>
2024-01-09 17:58:04 +0100

    Join multiple executions

--- step2b_join_heuristics.py
+++ step2b_join_heuristics.py
@@ -17,22 +17,27 @@ def main(dirpath: str, prefix_filter: str) -> None:
         return None
 
     # Join models
-    join_stats = dict()
     print(f'Joining {len(filespaths)} files...')
-    for i, filepath in enumerate(filespaths, 1):
+    all_stats = dict()
+    for fi, filepath in enumerate(filespaths, 1):
         filename = utils.get_filename_from_filepath(filepath)
-        print(f'{i} ', end='', flush=True)
-
+        print(f'{fi} ', end='', flush=True)
+        
         with open(filepath, newline=os.linesep, encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile, delimiter=',', quotechar='"', skipinitialspace=True)
-            for row in reader:
+            for i, row in enumerate(reader):
+                join_stats = dict()
+                if all_stats.get(i, None) is None:    
+                    all_stats[i] = join_stats
                 for header in reader.fieldnames:
                     if header == 'Time(s)':
-                        join_stats[header] = max(join_stats.get(header, 0.0), float(row[header]))
+                        all_stats[i][header] = max(all_stats[i].get(header, 0.0), float(row[header]))
                     else:
-                        join_stats[header] = join_stats.get(header, 0) + int(row[header])
+                        all_stats[i][header] = all_stats[i].get(header, 0) + int(row[header])
+        
     print()
-    print(f'{join_stats}')
+    #print(f'{join_stats}')
+    print(f'{all_stats}')
 
     # Serializing the join stats
     print(f'Serializing model...')
@@ -40,9 +45,11 @@ def main(dirpath: str, prefix_filter: str) -> None:
     filename = parts_name[0] + '_heuristics.csv'
     output_fmsans_filepath = os.path.join(dirpath, f'{filename}')
     with open(output_fmsans_filepath, 'w', newline=os.linesep, encoding='utf-8') as file:
-        writer = csv.DictWriter(file, join_stats.keys())
+        writer = csv.DictWriter(file, all_stats[0].keys())
         writer.writeheader()
-        writer.writerow(join_stats)
+        for r in all_stats:
+            print(all_stats[r])
+            writer.writerow(all_stats[r])
    
     print(f'Heuristics stats saved in {output_fmsans_filepath}')
 
